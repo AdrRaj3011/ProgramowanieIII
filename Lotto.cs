3e5108163c210ee47ef104f29c2e787c897c87c0
AdrRaj3011 <32573310+AdrRaj3011@users.noreply.github.com>
2017-10-18 22:28:26 +0200

    Add files via upload

---
+++ Lotto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var liczba = Enumerable.Range(0, 49).Select(x => ++x).OrderBy(x => Guid.NewGuid()).Take(6).ToList();
+            foreach (var item in liczba)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
